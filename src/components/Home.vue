9078142582c79ea8ab3a93396ab4f7da693dacaa
balcse <cserpesbalazs@gmail.com>
2025-09-12 16:49:24 +0200

    fix: removed client-side routing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
-    <a href="/schwarzplan/setup">Setup</a>
-    <p></p>
-    <a href="/schwarzplan/play">Play</a>
+    <div v-if="currentView === 'home'">
+        <p>Select an option to get started:</p>
+    <button @click="currentView = 'setup'">Setup</button>
+    <button @click="currentView = 'play'">Play</button>
+    </div>
+    <button v-if="currentView !== 'home'" @click="currentView = 'home'">Back to Home</button>
+    <Setup v-if="currentView === 'setup'" />
+    <Play v-if="currentView === 'play'" />
 </template>
+
+<script lang="ts" setup>
+import Setup from './Setup.vue';
+import Play from './Play.vue';
+import {ref} from 'vue';
+
+const currentView = ref('home');
+
+</script>
