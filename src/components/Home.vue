307ce45dab6c396e63ee3208af92465674801c58
balcse <cserpesbalazs@gmail.com>
2026-04-14 18:28:23 +0200

    feat: restyled buttons, added license info to footer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,17 @@
 <template>
+    <body>
     <div v-if="currentView === 'home'">
+    <h1>Schwarzplan: Städte und Bauepochen erkennen</h1>
+    <p class="footer">Erstellt von Balázs Cserpes und Veronika Halbert | <a href="https://github.com/balcse/schwarzplan/blob/5e65aea365c4e9832057664a7424746d29e66df9/LICENSE" target="_blank">Lizenz</a>
+    </p>
     <Button @click="currentView = 'setup'">Spiel erstellen</Button>
+    <br/>
     <Button @click="currentView = 'play'">Spiel starten</Button>
     </div>
     <Button v-if="currentView !== 'home'" @click="goToHome"><i class="pi pi-home"></i></Button>
     <Setup v-if="currentView === 'setup'" />
     <Play v-if="currentView === 'play'" />
+    </body>
 </template>
 
 <script lang="ts" setup>
@@ -26,3 +32,34 @@ function goToHome() {
 }
 
 </script>
+
+<style>
+body {
+padding: 1em;
+}
+
+button {
+margin-right: 1em;
+margin-bottom: 1em;
+background-color: rgb(255, 255, 255) !important;
+border-color: rgb(39, 39, 39) !important;
+border: 2pt solid !important;
+color: rgb(0, 0, 0) !important;
+}
+
+button:hover {
+    background-color: rgb(218, 218, 218) !important;
+    transition: 50ms !important;
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    right: 15;
+}
+
+.footer > a {
+    color: black;
+}
+
+</style>
